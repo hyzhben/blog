eb0607848cb3f40c60ec109d5b990240b2c562a8
zjm <2434153436@qq.com>
2020-04-29 18:34:06 +0800

    修改项目打包的bug

---
+++ blog-starter-swagger/src/main/java/com/blog/swagger/BlogStarterSwaggerApplication.java
@@ -0,0 +1,12 @@
+package com.blog.swagger;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogStarterSwaggerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BlogStarterSwaggerApplication.class, args);
+    }
+}
