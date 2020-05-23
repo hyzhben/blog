e42d9a70391fd6c56a25c5aba40fccf70c8a09e5
jean-zhou0512 <2434153436@qq.com>
2020-05-23 11:35:08 +0800

    生成blog_article、blog_article_type的实体类和mybatis映射文件等

---
+++ blog-starter-core/src/main/java/com/blog/core/system/service/BlogArticleService.java
@@ -0,0 +1,7 @@
+package com.blog.core.system.service;
+
+import com.blog.core.base.Service;
+import com.blog.core.system.dto.BlogArticle;
+
+public interface BlogArticleService  extends Service<BlogArticle> {
+}
