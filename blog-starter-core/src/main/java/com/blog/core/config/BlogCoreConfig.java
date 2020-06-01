06ae1b6a9c6420769d71eb6a40f63e13960141ff
zjm <2434153436@qq.com>
2020-06-01 15:23:14 +0800

    删除旧项目

--- blog-starter-core/src/main/java/com/blog/core/config/BlogCoreConfig.java
+++
@@ -1,7 +0,0 @@
-package com.blog.core.config;
-
-/*@Configuration
-@EnableAspectJAutoProxy(exposeProxy = true)
-@PropertySource(value = "classpath:application.properties")*/
-public class BlogCoreConfig {
-}
--- blog-starter-core/src/main/java/com/blog/core/system/service/IBlogService.java
+++
@@ -1,8 +0,0 @@
-package com.blog.core.system.service;
-
-import java.util.Map;
-
-public interface IBlogService {
-
-    public void createBlogArticle(Map<String,Object> paramMap);
-}
